cb993bba0611103897c741ff2dfc318e44aad030
Curtis Sand <curtis@curtissand.com>
2014-06-09 08:04:57 -0600

    converted update-site.sh script into daemon

--- update-site.sh
+++ update-site.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
 
-date
+update () {
+    date
+
+    echo pulling new updates...
+    git pull --rebase
+    git submodule update
+
+    if [[ $? -ne 0 ]] ; then
+        echo "git pull failed :("
+        exit;
+    fi
+
+
+    echo building and uploading...
+    make rsync_upload
+    rc=$?
+
+    date
+    return $rc
+}
 
 echo entering repository...
 pushd $(dirname $0)
 
-echo pulling new updates...
-git pull --rebase
-git submodule update
-
-if [[ $? -ne 0 ]] ; then
-    echo "git pull failed :("
-    exit;
-fi
-
-
-echo building and uploading...
-make rsync_upload
-rc=$?
-
-echo exiting...
-
-date
-
-exit $rc
+while true; do
+    update
+    echo "returncode: $?";
+    echo "1800 seconds until next update...";
+    sleep 1800;
+done
