c1fb1175fd9fd722cb5ef5692723fc27c62b17c6
Curtis Sand <curtis@curtissand.com>
2014-06-15 21:47:53 -0600

    Fixed up git_log.sh task to make the git pages better
    Now the actual git log output is pre-formatted text so it looks correct.
    Also, replacing @ with <AT> to make email address scraping harder.
    Added an end of log statement to make that obvious.

--- tasks/git_logs.sh
+++ tasks/git_logs.sh
@@ -2,40 +2,50 @@
 
 TASK=$(basename $0)
 DEST=$1/pages
+REPOS="blog fret"
 
-write_header () {
-    echo "========"
-    echo "$1 log"
-    echo "========"
-    echo ""
+get_header () {
+    echo -e "$1 log\n=================\n\n"
     echo ":date: $(date +"%Y-%m-%d %H:%M")"
     echo ":summary: A log of activity in repository $1."
-    echo -e "\n----\n"
+    echo -e "\n----\n\n.. raw:: html\n\n    <pre>\n"
 }
 
 get_log () {
     if [[ ! -z $1 ]]; then
         pushd $1 &>/dev/null;
     fi
-    git log
+    # replace @ to make email address scraping harder
+    git log | sed -e 's/@/<AT>/g'
     if [[ ! -z $1 ]]; then
         popd &>/dev/null;
     fi
 }
 
+get_footer () {
+    echo -e "\n.. raw:: html\n\n    </pre>\n\n[End of log]"
+}
+
+write_log_page () {
+    local repo=$1
+    local file=$DEST/$repo.rst
+    echo "$TASK: writing $repo log..."
+    touch $file
+    get_header "$repo" &> $file
+    get_log &>> $file
+    get_footer &>> $file
+}
+
+
 echo "$TASK: Writing Git Log Pages..."
 
 if [[ ! -d $DEST ]]; then
-    echo "$TASK: mkdir $DEST"
-    mkdir -p $DEST
+    echo "$TASK: mkdir $DEST";
+    mkdir -p $DEST;
 fi
 
-echo "$TASK: writing blog log..."
-touch $DEST/blog.rst
-write_header "blog" &> $DEST/blog.rst
-get_log &>> $DEST/blog.rst
+for repo in $REPOS; do
+    write_log_page $repo;
+done
 
-echo "$TASK: writing fret log..."
-touch $DEST/fret.rst
-write_header "fret" &> $DEST/fret.rst
-get_log "./fret" &>> $DEST/fret.rst
+echo "$TASK: done"
