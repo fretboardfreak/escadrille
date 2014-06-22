d441e1cdea1185fe7378a946dcc67a2d8477efb5
Curtis Sand <curtis@curtissand.com>
2014-06-21 19:26:21 -0600

    fixed up the git_logs.sh task so it works better

--- tasks/git_logs.sh
+++ tasks/git_logs.sh
@@ -5,35 +5,42 @@ DEST=$1/pages
 REPOS="blog fret"
 
 get_header () {
-    echo -e "$1 log\n=================\n\n"
+    (echo -e "$1 log\n=================\n\n"
     echo ":date: $(date +"%Y-%m-%d %H:%M")"
     echo ":summary: A log of activity in repository $1."
-    echo -e "\n----\n\n.. raw:: html\n\n    <pre>\n"
+    echo -e "\n----\n\n.. raw:: html\n\n    <pre>\n") > $2
 }
 
 get_log () {
     if [[ ! -z $1 ]]; then
+        echo "$TASK: changing dirs to $1..."
         pushd $1 &>/dev/null;
     fi
     # replace @ to make email address scraping harder
-    git log | sed -e 's/@/<AT>/g'
+    git log | sed -e 's/@/<AT>/g' &>> $2
     if [[ ! -z $1 ]]; then
-        popd &>/dev/null;
+        echo -n "$TASK: changing back to "
+        popd
     fi
 }
 
 get_footer () {
-    echo -e "\n.. raw:: html\n\n    </pre>\n\n[End of log]"
+    echo -e "\n.. raw:: html\n\n    </pre>\n\n[End of log]" >> $1
 }
 
 write_log_page () {
-    local repo=$1
-    local file=$DEST/$repo.rst
+    local repo="$1"
+    local file="$(readlink -f $DEST/$repo.rst)"
+    local dir="$1"
+    if [[ $dir == "blog" ]]; then
+        dir=".";
+    fi
+    dir=$(readlink -f $dir)
     echo "$TASK: writing $repo log..."
     touch $file
-    get_header "$repo" &> $file
-    get_log &>> $file
-    get_footer &>> $file
+    get_header $repo $file
+    get_log $dir $file
+    get_footer $file
 }
 
 
@@ -44,8 +51,8 @@ if [[ ! -d $DEST ]]; then
     mkdir -p $DEST;
 fi
 
-for repo in $REPOS; do
-    write_log_page $repo;
+for rp in $REPOS; do
+    write_log_page $rp;
 done
 
 echo "$TASK: done"
