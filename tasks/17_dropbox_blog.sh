932f59a6e3dc9c93178aee06ccfbcdecf60f5d55
Curtis Sand <curtis@curtissand.com>
2015-12-02 01:00:17 -0700

    tasks: added dropbox blog dir task
    The dir in dropbox serves as a way to easily put draft files in a place
    that can be picked up by the server that builds my website

---
+++ tasks/17_dropbox_blog.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+TASK_NAME=$(basename $0)
+DEST=$1
+
+die () {
+    echo "$TASK_NAME: $2";
+    exit $1
+}
+
+HOSTNAME=$(hostname)
+if [[ $HOSTNAME == "csand-fedora-vm" ]]; then
+    SRC="/home/csand/Dropbox/blog"
+elif [[ $HOSTNAME == "shiny" ]]; then
+    SRC="/home/csand/Dropbox/blog"
+elif [[ $HOSTNAME == "obsidian" ]]; then
+    SRC="/home/csand/Dropbox/blog"
+elif [[ $HOSTNAME == 'fedora-vm' ]]; then
+    SRC="/home/csand/storage/Dropbox/blog"
+else
+    echo "AAHHH, DONT KNOW WHAT TO DO WITH HOST `hostname`"
+fi
+
+test -z $SRC && die 1 "No source path defined for $HOSTNAME"
+
+echo "$TASK_NAME: copying blog: $SRC $DEST"
+rsync -haP --no-whole-file --inplace --exclude template.rst $SRC $DEST
+echo "$TASK_NAME: done"
