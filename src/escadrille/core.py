e91fe6df4ddb018b57ae2d83fab40d7ce18dadd2
Curtis Sand <curtis@curtissand.com>
2017-02-07 11:11:35 -0700

    escadrille.py: rename to escadrille.core and convert from script to module
    Setuptools can automatically create console scripts that call a given
    main method. So this moves the script style logic into a method that
    setuptools can call. Other front-end scripts can be added that simply
    import and call the new cli_main() method.

--- src/escadrille/core.py
+++ src/escadrille/core.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 # Copyright 2016-2017 Curtis Sand <curtissand@gmail.com>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,33 +11,21 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""The main escadrille script."""
+"""The core escadrille business logic."""
 import sys
 
-from lib.cmdline import UserInterface
-from lib.verbosity import vprint
-from lib.verbosity import dprint
-from lib.config import ConfigFile
-from lib.tasks import load_tasks
+from .cmdline import UserInterface
+from .verbosity import vprint
+from .verbosity import dprint
+from .config import ConfigFile
+from .tasks import load_tasks
+from .version import VERSION
 
 
-VERSION = "0.1"
-
-
-def main():
-    """Main method for escadrille.py"""
-    user_interface = UserInterface(version=VERSION)
-    options = user_interface.parse_cmd_line()
-
-    dprint('loading configuration...')
-    config_file = ConfigFile(options.config)
-    config_file.load()
-    tasks = load_tasks()
-    if options.default_config:
-        config_file.print_default_config(tasks)
-        return 0
-    if options.debug_config:
-        return user_interface.print_config_debug(options)
+def main(config_file, tasks=None):
+    """Main business logic for Escadrille."""
+    if tasks is None:
+        tasks = load_tasks()
     for task_name in config_file.enabled_tasks:
         task = tasks[task_name](config_file=config_file)
         task()
@@ -57,17 +44,29 @@ def main():
     return 0
 
 
-if __name__ == '__main__':
+def cli_main():
+    """Escadrille's main entry point for CLI usage."""
     try:
-        sys.exit(main())
+        user_interface = UserInterface(version=VERSION)
+        options = user_interface.parse_cmd_line()
+        dprint('loading configuration...')
+        config_file = ConfigFile(options.config)
+        config_file.load()
+        tasks = load_tasks()
+        if options.default_config:
+            config_file.print_default_config(tasks)
+            return 0
+        if options.debug_config:
+            return user_interface.print_config_debug(options)
+        sys.exit(main(config_file, tasks))
     except SystemExit:
         sys.exit(0)
     except KeyboardInterrupt:
         print('...interrupted by user, exiting.')
         sys.exit(1)
     except Exception as exc:
-        import lib.verbosity
-        if lib.verbosity.DEBUG:
+        import escadrille.verbosity
+        if escadrille.verbosity.DEBUG:
             raise
         else:
             print(exc)
