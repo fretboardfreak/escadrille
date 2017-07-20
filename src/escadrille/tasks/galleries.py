da116af943939901820d9e74af92a1cca3b02ca6
Curtis Sand <curtis@curtissand.com>
2017-07-20 13:04:57 -0700

    task galleries: convert gallery pages to use lightbox2
    Also, optionally use smaller thumbnail pictures in a subdirectory called
    "thumbs" if they are present.

--- src/escadrille/tasks/galleries.py
+++ src/escadrille/tasks/galleries.py
@@ -17,6 +17,7 @@ import os
 from collections import OrderedDict
 
 import escadrille.rst as rst
+from escadrille.verbosity import dprint
 
 from .core import Task
 from .options import OutputDirOpt
@@ -64,7 +65,25 @@ class GalleriesTask(OutputDirOpt, Task):
             self.galleries = galleries
         self.load_stubs()
 
-    def write_stubbed_gallery(self, title, media):
+    def write_content(self, fout, title, path, fnames, thumbs=False):
+        """Write the actual image content for the gallery pages."""
+        for image in fnames:
+            image_path = os.path.join(path, image)
+            if thumbs:
+                thumb_path = os.path.join(path, 'thumbs', image)
+                if (thumb_path.endswith('jpg') or
+                        thumb_path.endswith('JPG')):
+                    thumb_path = thumb_path[:-3] + 'png'
+                fout.write('    <a href="%s" data-lightbox="%s">'
+                           '<img src="%s"/></a>\n' %
+                           (image_path, title, thumb_path))
+            else:
+                thumb_path = image_path
+                fout.write('    <a href="%s" data-lightbox="%s">'
+                           '<img src="%s" style="width: 300px"/></a>\n' %
+                           (image_path, title, thumb_path))
+
+    def write_stubbed_gallery(self, title, path, fnames, thumbs=False):
         """Write an RST Gallery page using a pre-written stub file."""
         output_filename = os.path.join(self.output_dir,
                                        "%s.%s" % (title, self.suffix))
@@ -73,11 +92,10 @@ class GalleriesTask(OutputDirOpt, Task):
             with open(self.stubs[title], 'r') as stub_in:
                 fout.write(stub_in.read())
             fout.write('\n.. raw:: html\n\n    <div class="gallery">\n\n')
-            for image in media:
-                fout.write(rst.image_directive(image) + "\n")
+            self.write_content(fout, title, path, fnames, thumbs)
             fout.write('\n.. raw:: html\n\n    </div>')
 
-    def write_generated_gallery(self, title, media):
+    def write_generated_gallery(self, title, path, fnames, thumbs=False):
         """Write an RST Gallery page from scratch using fallback metadata."""
         output_filename = os.path.join(self.output_dir,
                                        "%s.%s" % (title, self.suffix))
@@ -93,8 +111,7 @@ class GalleriesTask(OutputDirOpt, Task):
             metadata['summary'] = ''
             fout.write(rst.metadata(metadata))
             fout.write('\n.. raw:: html\n\n    <div class="gallery">\n\n')
-            for image in media:
-                fout.write(rst.image_directive(image) + "\n")
+            self.write_content(fout, title, path, fnames, thumbs)
             fout.write('\n.. raw:: html\n\n    </div>')
 
     def __call__(self, *args, **kwargs):
@@ -103,25 +120,33 @@ class GalleriesTask(OutputDirOpt, Task):
         super().__call__(*args, **kwargs)
         tmp = self.galleries
         self.galleries = tmp if tmp.endswith('/') else tmp + '/'
-        for dirpath, _, fnames in os.walk(self.galleries):
+        for dirpath, dirnames, fnames in os.walk(self.galleries):
+            dprint('GALLERY DIRPATH=%s' % dirpath)
+            if dirpath.endswith('thumbs') or dirpath.endswith('thumbs/'):
+                continue
             common_prefix = os.path.commonprefix([dirpath, self.galleries])
             relative_path = dirpath[len(common_prefix):]
             if relative_path == "":
                 continue
             name = relative_path.replace('/', '-')
             media_prefix = './images'  # TODO: add to config file
+            thumbs = False
+            if "thumbs" in dirnames:
+                thumbs = True
+            cwd = '.'
+            if cwd in fnames:
+                fnames.remove(cwd)
             fnames.sort()
-            media = [os.path.join(media_prefix, relative_path, fname)
-                     for fname in fnames if not fname.startswith('.')]
-            if len(media) == 0:
+            path = os.path.join(media_prefix, relative_path)
+            if len(fnames) == 0:
                 self.vprint('Skipping %s, no images.' % name)
                 continue
             self.vprint('Creating gallery: %s.%s - %s images' %
-                        (name, self.suffix, len(media)))
+                        (name, self.suffix, len(fnames)))
             if name in self.stubs:
-                self.write_stubbed_gallery(name, media)
+                self.write_stubbed_gallery(name, path, fnames, thumbs)
             else:
-                self.write_generated_gallery(name, media)
+                self.write_generated_gallery(name, path, fnames, thumbs)
         self._set_status()
 
     def debug_msg(self):
